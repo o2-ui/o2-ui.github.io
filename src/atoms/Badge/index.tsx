5e2e893934223d3ad0d4ad2438ce29fec7a1763b
yunshin <2younsin@gmail.com>
2025-10-06 19:16:40 +0900

    FEAT: carmore-icon > release 버전 에 따라, NEW 뱃지 출력

---
+++ src/atoms/Badge/index.tsx
@@ -0,0 +1,38 @@
+import type { ComponentProps } from 'react';
+
+import React from 'react';
+
+import style from './style.module.css';
+
+import { mergeClassNames } from '@/utils/string-helper';
+
+type ThemeType = 'default' | 'primary';
+
+const getThemeClass = (theme: ThemeType) => {
+  switch (theme) {
+    case 'primary':
+      return style['theme-primary'];
+    case 'default':
+    default:
+      return style['theme-default'];
+  }
+};
+
+interface Props extends ComponentProps<'div'> {
+  label: string;
+  theme?: ThemeType;
+}
+
+const Badge = (props: Props) => {
+  const { className, label, theme = 'default', ...rest } = props;
+
+  const themeClass = getThemeClass(theme);
+
+  return (
+    <div className={mergeClassNames(style.layout, themeClass, className)} {...rest}>
+      <p className={style.label}>{label}</p>
+    </div>
+  );
+};
+
+export default Badge;
--- src/molecules/carmore-icon/IconItem/index.tsx
+++ src/molecules/carmore-icon/IconItem/index.tsx
@@ -6,21 +6,27 @@ import React from 'react';
 
 import style from './style.module.css';
 
+import Badge from '@/atoms/Badge';
 import Box from '@/atoms/Box';
+import { CAREMORE_ICON_LATEST_VERSION } from '@/contants/version';
 
 interface Props {
   icon: ComponentProps<typeof CarmoreIcon>['icon'];
+  version?: string;
   href?: string;
 }
 
 const IconItem = (props: Props) => {
-  const { href = '', icon } = props;
+  const { icon, version = '', href = '' } = props;
+
+  const isLatestReleased = version.includes(CAREMORE_ICON_LATEST_VERSION);
 
   return (
     <Link className={style.link} href={href}>
       <Box className={style.box} padding={'1rem'} bgColor={'gray-10-a-50'} hoverEffect>
+        {isLatestReleased && <Badge className={style['new-badge']} label={'NEW'} theme={'primary'} />}
         <CarmoreIcon width={65} height={65} icon={icon} />
-        <p className={style.iconLabel}>{icon.toString()}</p>
+        <p className={style['icon-label']}>{icon.toString()}</p>
       </Box>
     </Link>
   );
