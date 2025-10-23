d0f73f30d25e5658d55c1fc4ad7aec6b312f274e
yunshin <2younsin@gmail.com>
2025-10-23 22:04:58 +0900

    FEAT: carmore-icon > profile 에 아이콘 출시버전 표기

--- src/molecules/carmore-icon/IconProfile/index.tsx
+++ src/molecules/carmore-icon/IconProfile/index.tsx
@@ -3,8 +3,11 @@ import type { ComponentProps } from 'react';
 import { CarmoreIcon } from '@o2-ui/carmore-icon';
 import React from 'react';
 
+import style from './style.module.css';
+
 import Box from '@/atoms/Box';
 import Flex from '@/atoms/Flex';
+import { ICON_ITEM_MAP } from '@/contants/carmore-icon';
 
 interface Props {
   icon: ComponentProps<typeof CarmoreIcon>['icon'];
@@ -13,12 +16,17 @@ interface Props {
 const IconProfile = (props: Props) => {
   const { icon } = props;
 
+  const { releasedVersion } = ICON_ITEM_MAP[icon];
+
   return (
-    <Box fullWidth padding={'4rem'} bgColor={'gray-10'}>
-      <Flex fullWidth alignItems={'center'} justifyContent={'center'}>
-        <CarmoreIcon icon={icon} width={'15rem'} height={'15rem'} />
-      </Flex>
-    </Box>
+    <Flex fullWidth alignItems={'flex-end'} direction={'column'} gap={'0.5rem'}>
+      <i className={style['version-label']}>released at: {releasedVersion}</i>
+      <Box fullWidth padding={'4rem'} bgColor={'gray-10'}>
+        <Flex fullWidth alignItems={'center'} justifyContent={'center'}>
+          <CarmoreIcon icon={icon} width={'15rem'} height={'15rem'} />
+        </Flex>
+      </Box>
+    </Flex>
   );
 };
 
